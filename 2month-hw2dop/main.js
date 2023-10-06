e235ddff37c39f526168865653e604640a3e1dc6
asilbek08ro <146197621+asilbek08ro@users.noreply.github.com>
2023-10-06 19:50:19 +0600

    Update main.js

--- 2month-hw2dop/main.js
+++ 2month-hw2dop/main.js
@@ -1,20 +1,20 @@
-// Создать массив из значений 100, 55, 15, 300, 20.
+
 const numbers = [100, 55, 15, 300, 20];
 
-// Инициализировать переменную для хранения суммы.
+
 let sum = 0;
 
-// Пройти по массиву и суммировать значения с пояснениями в консоли.
+
 for (let i = 0; i < numbers.length; i++) {
     const currentNumber = numbers[i];
     console.log(`${sum} + ${currentNumber} = ${sum + currentNumber}`);
     sum += currentNumber;
 }
 
-// Вывести результат суммирования в консоль.
+
 console.log(`Итоговая сумма: ${sum}`);
 
-// Создать объект с 500 ключами (просто для дополнительной практики).
+
 const objectWith500Keys = {};
 
 for (let i = 0; i < 500; i++) {
